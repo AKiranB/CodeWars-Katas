86354b14c7a37958c9c90a58e7fcd0c8f596ffc8
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2021-10-14 10:48:25 +0200

    5kyRot15

--- 5kyuRot15.js
+++ 5kyuRot15.js
@@ -1,27 +1,10 @@
-const rot = (string) => {
+const rot13 = (message) => {
     let resArr = []
-    const alphabeticSetA = 'abcdefghijklm'.split("")
-    const alphabeticSetB = 'nopqrstuvwxyz'.split('')
-
-    string.split('').forEach(char => {
-
-        if (char.toLowerCase()) {
-            if (alphabeticSetA.includes(char)) {
-                resArr.push(alphabeticSetB[alphabeticSetA.indexOf(char)])
-            } else if (alphabeticSetB.includes(char)) {
-                resArr.push(alphabeticSetA[alphabeticSetB.indexOf(char)])
-            }
-        } else {
-            if (alphabeticSetA.includes(char)) {
-                resArr.push(alphabeticSetB[alphabeticSetA.indexOf(char)].toUpperCase())
-            } else if (alphabeticSetB.includes(char)) {
-                resArr.push(alphabeticSetA[alphabeticSetB.indexOf(char)].toUpperCase())
-            }
-        }
-
-        //i think we can write whtehr the symbol is part of a-z in the above code, break down all seperate conditions
+    const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXWZ"
+    const cipher = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+    message.split('').forEach(char => {
+        if (!alphabet.includes(char)) { return resArr.push(char) }
+        resArr.push(alphabet[cipher.indexOf(char)])
     })
-
     return resArr.join("")
 }
-
