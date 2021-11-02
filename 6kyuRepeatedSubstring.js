90c17ce8b5aa36f1885ba41a5cb7e12a0f765472
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2021-11-02 15:09:56 +0100

    added 6kyu repeated substring

---
+++ 6kyuRepeatedSubstring.js
@@ -0,0 +1,22 @@
+// For a given nonempty string s 
+// find a minimum substring t and 
+// the maximum number k, such that 
+// the entire string s is equal to t 
+// repeated k times. The input string consists of lowercase 
+// latin letters. 
+// Your function should return a tuple (in Python) (t, k) 
+// or an array (in Ruby and JavaScript) [t, k]
+
+
+function f(s) {
+    let subString = s[0];
+    for (let i = 1; i < s.length; i++) {
+        let re = new RegExp(subString)
+        if (!re.test(s.split('').slice(i, i + subString.length).join(''))) {
+            subString += s[i]
+        } else {
+            break
+        }
+    }
+    return [subString, s.split(subString).length - 1]
+}
