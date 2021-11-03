b45a702e487f940c0b6a3a2844fe60add57edb87
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2021-11-03 11:09:53 +0100

    5kyuMoveZeroes

---
+++ 5kyuMoveZeroestoEnd.js
@@ -0,0 +1,15 @@
+// Write an algorithm that takes an array and 
+// moves all of the zeros to the end, 
+// preserving the order of the other elements.
+var moveZeros = function (arr) {
+    let tmpArr = []
+    arr.forEach(el => {
+        if (el !== 0) {
+            tmpArr.push(el)
+        }
+    })
+    for (let i = 0; i < arr.join('').split(0).length - 1; i++) {
+        tmpArr.push(0)
+    }
+    return tmpArr
+}
